b2f8ff01bd1bd44ab33b073291c5d9cd239d4bfe
cmronk <christinemronk@gmail.com>
2018-09-25 20:10:33 -0500

    initial commit with create-react-app, added components folder and testing reactstrap

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Button color="secondary">Hello!</Button>
+            </div>
+        )
+    }
+}
+
+export default Header;
